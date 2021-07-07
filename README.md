b79be2272d5ea332bc63b1d19fbdf5324cf76ac1
MauVal96 <m.valadezolmos@ugto.mx>
2021-07-07 18:19:37 -0500

    Conditional Statements: If,Elif,Else

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Examples and exercises from my Python learning path
 ## Files
 
 1. Currency converter MXN -> USD
+2. Conditional Statements -> If,Elif,Else
 
 **Made with 💙 by MauVal96**
---
+++ conditional.py
@@ -0,0 +1,19 @@
+# Conditional Statements in Python
+
+# If-else statements
+age = int(input("Input your age: "))
+
+if age > 17:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+#If-elif-else statements
+number = int(input("Input a number: "))
+
+if number > 5:
+    print("Is greater than 5")
+elif number == 5:
+    print("Is equal to 5")
+else:
+    print("Is less than 5")
